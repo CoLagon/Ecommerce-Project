55d26065793896cf6327442dd6bcb6d2c2562ad1
Cole Logan <cole@Coles-MacBook-Air.local>
2017-07-27 15:08:03 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,298 @@
+
+//array of object literals
+var JEANS = [
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 3
+    },
+    {
+      SKU: 1234,
+      title: "SAINT VERNON",
+      fit: "TAPER FIT",
+      color: "blue",
+      size: [ 28, 30, 32 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/rustic-dime-saint-vernon-taper-fit-1_1024x1024-1_1024x1024.jpg?v=1492608261%22", "https://cdn.shopify.com/s/files/1/0452/6221/products/rustic-dime-saint-vernon-taper-fit-2_1024x1024.jpg?v=1492608262"],
+      description: "Our Saint Vernon Taper Fit denim features distressed and tattered details. Each pair is hand cut and sanded for a unique look. 100% cotton bleached american denim. Made in Los Angeles.",
+      id: 1
+    },
+    {
+      SKU: 4321,
+      title: "BLACK",
+      fit: "SKINNY FIT",
+      color: "black",
+      size: [ 28, 30, 32 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/105black_1024x1024.jpg?v=1492608011", "https://cdn.shopify.com/s/files/1/0452/6221/products/105black_b_1024x1024.jpg?v=1492608012","https://cdn.shopify.com/s/files/1/0452/6221/products/105black_c_1024x1024.jpg?v=1492608013"],
+      description: "Our signature Black Skinny Fit denim is a fan favorite. Made from a garment dyed 98% cotton 2% spandex, these jeans are perfect for everyday wear. Made in Los Angeles.",
+      id: 2
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 4
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 5
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 6
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 7
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 8
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 9
+    },
+    {
+      SKU: 1324,
+      title: "RUSTIC DIME SHREDDED JEANS",
+      fit: "TAPER FIT",
+      color: "black",
+      size: [ 30, 32, 34 ],
+      image: ["https://cdn.shopify.com/s/files/1/0452/6221/products/110raw_1024x1024.jpg?v=1492608239"],
+      description: "Get a stylish street look loaded with comfort with the new Biker Black Shredded jeans from Rustic Dime. A stylish tapered fit will have you looking your best in the black colorway that features custom knee panels and hand cut tears on the front for a shredded look and a cotton-spandex construction for a comfortable fit with plenty of stretch.",
+      id: 10
+    }
+];
+
+function DebugBrother(props) {
+  return (
+    <div className="column">
+      <img  src={props.image}/>
+    </div>
+  );
+}
+
+//should probably use a stateful component for this
+function Debug(props) {
+  console.log(props.jean);
+  return (
+    <div className="columns">
+    {
+      props.jean.map(function(send, index) {
+        console.log(send);
+        return <DebugBrother index={index} image={send.image} key={index}/>
+      })
+    }
+
+    </div>
+  );
+}
+function NewPage() {
+  return(
+    <div>
+      <h1> this is the new page, mate </h1>
+    </div>
+  );
+}
+function ButtonList(props) {
+  return (
+      <li onClick={function() {props.onSize(props.item, props.index);}}><a>{props.item}</a></li>
+  );
+}
+function ButtonDrop(props) {
+  return (
+    <div className="has-dropdown">
+    <button className="button is-medium" onClick={function() {props.onButtonDrop(props.index); console.log("drop");}}>{props.size}</button>
+      <div className="dropdown box" id={props.toggle} name="toggle">
+        <ul >
+        {
+          props.sizes.map(function(size, index) {
+            return <ButtonList key={index} index={props.index} item={size} onSize={props.onSizeSelect}/>
+          })
+        }
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+var DisplayImage = React.createClass({
+  sizeSelect: function(size, index) {
+    this.setState({ size: size})
+    document.getElementsByName("toggle")[index].style.display = "none";
+  },
+  ProductPage: function() {
+    ReactDOM.render(<NewPage/>, document.getElementById("root"));
+  },
+  dropButton: function(index) {
+    var element = document.getElementsByName("toggle")[index];
+    var id = element.getAttribute("id");
+    if(id === "hide") {
+      element.style.display = "none";
+      this.setState({ toggle: "show" })
+    }
+    if(id === "show")
+    {
+      element.style.display = "block";
+      this.setState({toggle: "hide"})
+    }
+  },
+  componentDidMount: function() {
+    var element = document.getElementsByName("toggle")[this.props.index];
+    var id = element.getAttribute("id");
+
+    element.style.display = "none";
+  },
+  flipImage: function(len) {
+    var loopImage = function(i) {
+      if(i < len) {
+        this.Timeout = setTimeout(function() {
+          this.setState({image: this.props.image[i]})
+          loopImage(i + 1);
+        }.bind(this), 500)
+      }
+    }.bind(this);
+    loopImage(1);
+  },
+  toDefault: function() {
+    clearTimeout(this.Timeout);
+    this.setState(this.getInitialState());
+  },
+  getInitialState: function() {
+    return {
+            image: this.props.image,
+            toggle: "show",
+            size: "size"
+           }
+  },
+  render: function() {
+    return (
+        <div className="column">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-2by3">
+                    <img href="localhost:8080/ProductPage" src={this.state.image} onMouseOver={ function() {
+                      var len = this.state.image.length;
+                      this.flipImage(len);
+                    }.bind(this) } onMouseOut={function() {this.toDefault();}.bind(this)} onClick={ function() {this.ProductPage()}.bind(this)}/>
+                </figure>
+              </div>
+            </div>
+            <div>
+              {this.props.title}
+              <ButtonDrop
+                  toggle={this.state.toggle}
+                  sizes={this.props.size}
+                  index={this.props.index}
+                  size={this.state.size}
+                  onSizeSelect={function(size, index) { this.sizeSelect(size, index)}.bind(this)}
+                  onButtonDrop={function(index) { this.dropButton(index)}.bind(this)}/>
+            </div>
+        </div>
+    );
+  }
+})
+
+//Object literal or Class with properties and methods
+//this class holds function expressions(methods)
+var Ecommerce = React.createClass({
+  propTypes: {
+    initialJeans: React.PropTypes.arrayOf(React.PropTypes.shape({
+      SKU: React.PropTypes.number.isRequired,
+      title: React.PropTypes.string.isRequired,
+      fit: React.PropTypes.string.isRequired,
+      color: React.PropTypes.string.isRequired,
+      size: React.PropTypes.arrayOf(React.PropTypes.number.isRequired),
+      image: React.PropTypes.arrayOf(React.PropTypes.string.isRequired),
+      description: React.PropTypes.string.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired
+  },
+  getInitialState: function() {
+    //split the jeans into an array of 4 and put the remaining in the last array
+    // use the splice and the push or unshift method
+    var jeans = this.props.initialJeans;
+    var arrOfArr = [];
+    var columns = 0;
+    var increase = 4;
+    for(var i = 0; i < jeans.length; i++) {
+      arrOfArr.push(jeans.splice(0, 5));
+      // columns += increase
+      // if (i < columns) {
+      //   arrOfArr.push(jeans.splice(0, 2));
+      // }
+
+    }
+    console.log(arrOfArr);
+    console.log(jeans);
+    return {
+      jeans: arrOfArr,
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        {this.state.jeans.map(function(jean,index) {
+            return <Debug jean={jean} index={index} key={index}/>
+        })
+
+      }
+      </div>
+    );
+  }
+});
+// {this.state.jeans.map(function(jean, index) {
+//   return (
+//       <DisplayImage
+//         SKU={jean.SKU}
+//         title={jean.title}
+//         fit={jean.fit}
+//         color={jean.color}
+//         size={jean.size}
+//         image={jean.image}
+//         description={jean.description}
+//         key={jean.id}
+//         index={index}/>
+//   );
+// }.bind(this))}
+
+ReactDOM.render(<Ecommerce initialJeans={JEANS}/>, document.getElementById("root"));
